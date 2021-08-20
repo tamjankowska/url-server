9e7748a880a6d0b0b6e203fbc87f9a19c36c68cb
Tam <tam.jankowska@gmail.com>
2021-08-20 12:29:13 +0100

    server port config and get request response

--- index.js
+++ index.js
@@ -9,6 +9,17 @@ const app = express()
 // connect to db
 connection()
 
+// establish port
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+    console.log(`Server now running on port ${port}`)
+})
+
+app.get('/', (req, res) => {
+    res.send('Here we are!')
+}
+)
+
 // cors config
 app.use(
     cors({
