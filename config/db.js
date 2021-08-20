8af08c06433099c81e5d017768e919f0afe90e6c
Tam <tam.jankowska@gmail.com>
2021-08-20 12:18:21 +0100

    try/catch in place for db connection

--- config/db.js
+++ config/db.js
@@ -3,9 +3,13 @@ require('dotenv').config()
 
 const connection = async () => {
     try {
-        
+        await mongoose.connect(process.env.MONGO_CONNECTION_STRING, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        console.log('MongoDB is connected, huzzah!')
     } catch (error) {
-        
+        console.log(error)
     }
 }
 
