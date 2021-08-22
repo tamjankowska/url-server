80e015dfe7096ca81a8ed624d56d1f8293c0d955
Tam <tam.jankowska@gmail.com>
2021-08-22 21:47:58 +0100

    routes modified

--- routes/UrlRouter.js
+++ routes/UrlRouter.js
@@ -11,13 +11,19 @@ router.get('/urls', async (req, res) => {
 router.post('/urls/create', async (req, res) => {
     try {
         const url = await new Url(req.body).save()
-        res.status(200).json({ message: 'Url created', url: {
-            concatUrl: url.prefixUrl + url.urlId,
-            createdAt: url.createdAt
-        }})
+        res.status(200).json({ message: 'Url created', url})
     } catch (error) {
         res.status(500).json({ message: 'Unable to create url', error })
     }
+})
+
+router.get('/urls/:urlId', async (req, res) => {
+    try {
+        const url = await Urls.findOne({ urlId: req.params.urlId})
+        res.status(200).json(url)
+    } catch (error) {
+        res.status(404).json(error)
+    } // could add error handling here for other response codes: 400s, 500s,
 })
 
 module.exports = router
